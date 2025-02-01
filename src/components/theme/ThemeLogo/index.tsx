a79d4ad793b7fd3675569280ae5359a43001531e
async <1827951482@qq.com>
2025-02-02 00:37:24 +0800

    fix: 修复 system 主题 logo 展示错误

--- src/components/theme/ThemeLogo/index.tsx
+++ src/components/theme/ThemeLogo/index.tsx
@@ -5,6 +5,13 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function ThemeLogo() {
-  const { theme } = useTheme();
-  return <Image width={40} height={40} src={theme == "light" ? logoImages.LIGHT : logoImages.DARK} alt="logo" />;
+  const { theme, systemTheme } = useTheme();
+  return (
+    <Image
+      width={40}
+      height={40}
+      src={theme == "light" || (theme === "system" && systemTheme === "light") ? logoImages.LIGHT : logoImages.DARK}
+      alt="logo"
+    />
+  );
 }
