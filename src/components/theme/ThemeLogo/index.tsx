945c22e82d5700136f735b5c54350eafaf1c03b4
async <1827951482@qq.com>
2025-02-03 23:41:09 +0800

    fix: 修复 react-hydration-error 问题

--- src/components/theme/ThemeLogo/index.tsx
+++ src/components/theme/ThemeLogo/index.tsx
@@ -1,17 +1,23 @@
-"use client";
-
 import { logoImages } from "@/constants";
-import { useTheme } from "next-themes";
 import Image from "next/image";
 
 export default function ThemeLogo() {
-  const { theme, systemTheme } = useTheme();
   return (
-    <Image
-      width={40}
-      height={40}
-      src={theme == "light" || (theme === "system" && systemTheme === "light") ? logoImages.LIGHT : logoImages.DARK}
-      alt="logo"
-    />
+    <>
+      <Image
+        width={40}
+        height={40}
+        className="dark:hidden"
+        src={logoImages.LIGHT}
+        alt="logo"
+      />
+      <Image
+        width={40}
+        height={40}
+        className="hidden dark:block"
+        src={logoImages.DARK}
+        alt="logo"
+      />
+    </>
   );
 }
